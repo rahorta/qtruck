c035bf15d34c759e0fcd0e7a9ff9c01fc12a65b0
Rodolfo Goulart <rodolfogoulart89@gmail.com>
2022-08-21 18:37:34 -0300

    adding cypress-mongodb's dependency to fix the test scenario of signup.

--- tests/cypress.config.js
+++ tests/cypress.config.js
@@ -1,9 +1,11 @@
 const { defineConfig } = require('cypress')
 const { cypressBrowserPermissionsPlugin } = require('cypress-browser-permissions')
+const mongo = require('cypress-mongodb')
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
+      mongo.configurePlugin(on)
       config = cypressBrowserPermissionsPlugin(on, config)
       return config
       // implement node event listeners here
@@ -15,6 +17,10 @@ module.exports = defineConfig({
       browserPermissions: {
         notifications: 'allow',
         geolocation: 'allow'
+      },
+      mongodb: {
+        uri: 'mongodb+srv://qa:cademy@cluster0.4bju0o5.mongodb.net/QtruckDB?retryWrites=true&w=majority',
+        database: 'QtruckDB'
       }
     }
   },
--- tests/cypress/e2e/signup.cy.js
+++ tests/cypress/e2e/signup.cy.js
@@ -8,6 +8,10 @@ describe('Signup', ()=>{
             password: 'pwd123'
         }
 
+        cy.deleteMany({instagram: user.instagram}, { collection: 'users'}).then(res => { 
+            cy.log(res); 
+        })
+
         signupPage.register(user)
         signupPage.modal.haveText('Agora você pode recomendar e/ou avaliar Food trucks.')
 
