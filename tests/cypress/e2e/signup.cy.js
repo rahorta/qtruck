f47f878e0c410af7ae4aab60db4903d572662f0c
Rodolfo Goulart <rodolfogoulart89@gmail.com>
2022-08-23 19:36:20 -0300

    hacking Qtruck's API to add the route deleteUser

--- tests/cypress/e2e/signup.cy.js
+++ tests/cypress/e2e/signup.cy.js
@@ -1,19 +1,32 @@
 import signupPage from '../support/pages/Signup'
 
-describe('Signup', ()=>{
-    it('deve cadastrar novo usuário', () =>{
+describe('Signup', () => {
+    it('deve cadastrar novo usuário', () => {
         const user = {
             name: "Ronaldo Fenomeno",
             instagram: "@ronaldo9",
             password: 'pwd123'
         }
 
-        cy.deleteMany({instagram: user.instagram}, { collection: 'users'}).then(res => { 
+        /* cy.deleteMany({instagram: user.instagram}, { collection: 'users'}).then(res => { 
             cy.log(res); 
-        })
+        }) */
+
+        cy.apiResetUser(user.instagram)
 
         signupPage.register(user)
         signupPage.modal.haveText('Agora você pode recomendar e/ou avaliar Food trucks.')
 
+    })
+})
+
+
+Cypress.Commands.add('apiResetUser', (instagram) => {
+    cy.request({
+        url: 'http://localhost:3333/helpers-reset',
+        method: 'DELETE',
+        qs: { instagram: instagram }
+    }).then(response => {
+        expect(response.status).to.eql(204)
     })
 })
